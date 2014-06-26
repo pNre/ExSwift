7a820353f0a69cd82bb6f00f4ade5128dfabdabd
Colin Eberhardt <colin.eberhardt@gmail.com>
2014-06-26 17:09:27 +0100

    Implemented many more operations

--- ExSwift/Sequence.swift
+++ ExSwift/Sequence.swift
@@ -16,12 +16,58 @@ extension SequenceOf {
         return generator.next()
     }
     
+    func any (call: (T) -> Bool) -> Bool {
+        var generator =  self.generate();
+        while let nextItem = generator.next() {
+            if call(nextItem) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func get (index: Int) -> T? {
+        var generator =  self.generate();
+        for _ in 0..(index-1) {
+            generator.next()
+        }
+        return generator.next()
+    }
+    
+    func get (range: Range<Int>) -> SequenceOf<T> {
+        return self.skip(range.startIndex)
+            .take(range.endIndex - range.startIndex)
+    }
+    
+    func indexOf <U: Equatable> (item: U) -> Int? {
+        var index = 0;
+        for current in self {
+            if let equatable = current as? U {
+                if equatable == item {
+                    return index
+                }
+            }
+            index++
+        }
+        return nil
+    }
+    
     func skip (n:Int) -> SequenceOf<T> {
         var generator =  self.generate();
         for _ in 0..n {
             generator.next()
         }
         return SequenceOf(generator)
+    }
+    
+    func filter(includeElement: (T) -> Bool) -> SequenceOf<T> {
+        return SequenceOf(FilterSequence(self, includeElement))
+    }
+    
+    func reject (exclude: (T -> Bool)) -> SequenceOf<T> {
+        return self.filter {
+            return !exclude($0)
+        }
     }
     
     func skipWhile(condition:(T) -> Bool) -> SequenceOf<T> {
@@ -54,6 +100,56 @@ extension SequenceOf {
     func takeWhile (condition:(T?) -> Bool) -> SequenceOf<T>  {
         return SequenceOf(TakeWhileSequence(self, condition))
     }
+    
+    //TODO: methods that make sense to add to sequence
+    
+    // unique <T: Equatable> ()
+    // flatten <OutType> () -> OutType[]
+    
+    // DONE
+    
+    // reject (exclude: (Element -> Bool)) -> SequenceOf<Element>
+    // any (call: (Element) -> Bool) -> Bool
+    // get (range: Range<Int>) -> SequenceOf<T>
+    // get (index: Int) -> Element?
+    // indexOf <U: Equatable> (item: U) -> Int?
+    // first () -> T?
+    // takeWhile (condition:(Element?) -> Bool) -> SequenceOf<Element>
+    // take (n:Int) -> SequenceOf<Element>
+    // contains<T:Equatable> (item: T) -> Bool
+    // skip (n:Int) -> SequenceOf<T>
+    // skipWhile(condition:(T) -> Bool) -> SequenceOf<T>
+}
+
+// a sequence adapter that implements the 'filter' functionality
+struct FilterSequence<S:Sequence>: Sequence {
+    let sequence: S
+    let includeElement: (S.GeneratorType.Element) -> Bool
+    
+    init(_ sequence: S, _ includeElement: (S.GeneratorType.Element) -> Bool) {
+        self.sequence = sequence
+        self.includeElement = includeElement
+    }
+    
+    func generate() -> GeneratorOf<S.GeneratorType.Element> {
+        var generator = self.sequence.generate()
+        return GeneratorOf<S.GeneratorType.Element> {
+            var keepSkipping = true
+            var nextItem = generator.next()
+            while keepSkipping {
+                if let unwrappedItem = nextItem {
+                    keepSkipping = !self.includeElement(unwrappedItem)
+                } else {
+                    keepSkipping = false
+                }
+                
+                if (keepSkipping) {
+                    nextItem = generator.next()
+                }
+            }
+            return nextItem
+        }
+    }
 }
 
 // a sequence adapter that implements the 'take' functionality
@@ -80,7 +176,7 @@ struct TakeSequence<S:Sequence>: Sequence {
     }
 }
 
-// a sequence adapter that implements the 'take' functionality
+// a sequence adapter that implements the 'takeWhile' functionality
 struct TakeWhileSequence<S:Sequence>: Sequence {
     let sequence: S
     let condition: (S.GeneratorType.Element?) -> Bool
--- ExSwiftTests/ExSwiftSequenceTests.swift
+++ ExSwiftTests/ExSwiftSequenceTests.swift
@@ -73,4 +73,56 @@ class ExtensionsSequenceTests: XCTestCase {
         XCTAssertEqualObjects(Array(take), [1,2,3,4,5])
     }
     
+    func testIndexOf () {
+        XCTAssertEqual(SequenceOf(sequence).indexOf(2)!, 1)
+        XCTAssertNil(SequenceOf(sequence).indexOf(77))
+    }
+    
+    func testGet () {
+        XCTAssertEqual(SequenceOf(sequence).get(3)!, 3)
+        XCTAssertNil(SequenceOf(sequence).get(22))
+    }
+    
+    func testGetRange () {
+        var subSequence = SequenceOf(sequence).get(1..3)
+        XCTAssertEqualObjects(Array(subSequence), [2,3])
+        
+        subSequence = SequenceOf(sequence).get(0..0)
+        XCTAssertEqualObjects(Array(subSequence), [])
+    }
+    
+    func testGetRangeOutOfBounds () {
+        var subSequence = SequenceOf(sequence).get(10..15)
+        XCTAssertEqualObjects(Array(subSequence), [])
+    }
+    
+    func testAny () {
+        XCTAssertTrue(SequenceOf(sequence).any { $0 == 1 })
+        XCTAssertFalse(SequenceOf(sequence).any { $0 == 77 })
+    }
+    
+    func testFilter () {
+        var evens = SequenceOf(sequence).filter { $0 % 2 == 0 }
+        XCTAssertEqualObjects(Array(evens), [2,4])
+        
+        var odds = SequenceOf(sequence).filter { $0 % 2 == 1 }
+        XCTAssertEqualObjects(Array(odds), [1,3,5])
+        
+        var all = SequenceOf(sequence).filter { $0 < 10 }
+        XCTAssertEqualObjects(Array(all), [1,2,3,4,5])
+        
+        var none = SequenceOf(sequence).filter { $0 > 10 }
+        XCTAssertEqualObjects(Array(none), [])
+    }
+    
+    func testReject () {
+        var rejected = SequenceOf(sequence).reject { $0 == 3 }
+        XCTAssertEqualObjects(Array(rejected), [1,2,4,5])
+        
+        rejected = SequenceOf(sequence).reject { $0 == 1 }
+        XCTAssertEqualObjects(Array(rejected), [2,3,4,5])
+        
+        rejected = SequenceOf(sequence).reject { $0 == 10 }
+        XCTAssertEqualObjects(Array(rejected), [1,2,3,4,5])
+    }
 }
