571c1c8d3de70c957f29b84ea3bf5c814e34f807
Colin Eberhardt <colin.eberhardt@gmail.com>
2014-06-26 17:30:56 +0100

    Added some API documentation

--- ExSwift/Sequence.swift
+++ ExSwift/Sequence.swift
@@ -11,11 +11,20 @@ import Foundation
 
 extension SequenceOf {
 
+    /**
+    *  First element of the sequence
+    *  @return First element of the sequence if present
+    */
     func first () -> T? {
         var generator =  self.generate();
         return generator.next()
     }
     
+    /**
+    *  Checks if call returns true for any element of self
+    *  @param call Function to call for each element
+    *  @return True if call returns true for any element of self
+    */
     func any (call: (T) -> Bool) -> Bool {
         var generator =  self.generate();
         while let nextItem = generator.next() {
@@ -26,6 +35,11 @@ extension SequenceOf {
         return false
     }
     
+    /**
+    *  Object at the specified index if exists
+    *  @param index
+    *  @return Object at index in sequence, nil if index is out of bounds
+    */
     func get (index: Int) -> T? {
         var generator =  self.generate();
         for _ in 0..(index-1) {
@@ -34,11 +48,21 @@ extension SequenceOf {
         return generator.next()
     }
     
+    /**
+    *  Objects in the specified range
+    *  @param range
+    *  @return Subsequence in range
+    */
     func get (range: Range<Int>) -> SequenceOf<T> {
         return self.skip(range.startIndex)
             .take(range.endIndex - range.startIndex)
     }
     
+    /**
+    *  Index of the first occurrence of item, if found
+    *  @param item The item to search for
+    *  @return Index of the matched item or nil
+    */
     func indexOf <U: Equatable> (item: U) -> Int? {
         var index = 0;
         for current in self {
@@ -52,6 +76,10 @@ extension SequenceOf {
         return nil
     }
     
+    /**
+    *  Subsequence from n to the end of the sequence
+    *  @return Sequence from n to the end
+    */
     func skip (n:Int) -> SequenceOf<T> {
         var generator =  self.generate();
         for _ in 0..n {
@@ -60,16 +88,31 @@ extension SequenceOf {
         return SequenceOf(generator)
     }
     
-    func filter(includeElement: (T) -> Bool) -> SequenceOf<T> {
-        return SequenceOf(FilterSequence(self, includeElement))
+    /**
+    *  Filters the sequence only including items that match the test
+    *  @param include Function invoked to test elements for inclusion in the sequence
+    *  @return Filtered sequence
+    */
+    func filter(include: (T) -> Bool) -> SequenceOf<T> {
+        return SequenceOf(FilterSequence(self, include))
     }
     
+    /**
+    *  Opposite of filter
+    *  @param exclude Function invoked to test elements for exlcusion from the sequence
+    *  @return Filtered sequence
+    */
     func reject (exclude: (T -> Bool)) -> SequenceOf<T> {
         return self.filter {
             return !exclude($0)
         }
     }
     
+    /**
+    *  Skips the elements in the sequence up until the condition returns false
+    *  @param condition A function which returns a boolean if an element satisfies a given condition or not
+    *  @return Elements of the sequence starting with the element which does not meet the condition
+    */
     func skipWhile(condition:(T) -> Bool) -> SequenceOf<T> {
         var generator =  self.generate();
         var keepSkipping = true
@@ -83,6 +126,11 @@ extension SequenceOf {
         return SequenceOf(generator)
     }
     
+    /**
+    *  Checks if self contains the item object
+    *  @param item The item to search for
+    *  @return true if self contains item
+    */
     func contains<T:Equatable> (item: T) -> Bool {
         var generator =  self.generate();
         while let nextItem = generator.next() {
@@ -93,32 +141,22 @@ extension SequenceOf {
         return false
     }
 
+    /**
+    *  Returns the first n elements from self
+    *  @return First n elements
+    */
     func take (n:Int) -> SequenceOf<T> {
         return SequenceOf(TakeSequence(self, n))
     }
     
+    /**
+    *  Returns the elements of the sequence up until an element does not meet the condition
+    *  @param condition A function which returns a boolean if an element satisfies a given condition or not.
+    *  @return Elements of the sequence up until an element does not meet the condition
+    */
     func takeWhile (condition:(T?) -> Bool) -> SequenceOf<T>  {
         return SequenceOf(TakeWhileSequence(self, condition))
     }
-    
-    //TODO: methods that make sense to add to sequence
-    
-    // unique <T: Equatable> ()
-    // flatten <OutType> () -> OutType[]
-    
-    // DONE
-    
-    // reject (exclude: (Element -> Bool)) -> SequenceOf<Element>
-    // any (call: (Element) -> Bool) -> Bool
-    // get (range: Range<Int>) -> SequenceOf<T>
-    // get (index: Int) -> Element?
-    // indexOf <U: Equatable> (item: U) -> Int?
-    // first () -> T?
-    // takeWhile (condition:(Element?) -> Bool) -> SequenceOf<Element>
-    // take (n:Int) -> SequenceOf<Element>
-    // contains<T:Equatable> (item: T) -> Bool
-    // skip (n:Int) -> SequenceOf<T>
-    // skipWhile(condition:(T) -> Bool) -> SequenceOf<T>
 }
 
 // a sequence adapter that implements the 'filter' functionality
