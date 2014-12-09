693894879c273d40619719410767e4fc7993c208
Cenny Davidsson <cenny@me.com>
2014-12-09 12:46:20 +0100

    Added Charater extension
    Implemented toInt() method for Charaters and added test for it.

---
+++ ExSwift/Character.swift
@@ -0,0 +1,21 @@
+//
+//  Character.swift
+//  ExSwift
+//
+//  Created by Cenny Davidsson on 2014-12-08.
+//  Copyright (c) 2014 pNre. All rights reserved.
+//
+
+import Foundation
+
+public extension Character {
+    
+    /**
+        If the character represents an integer that fits into an Int, returns
+        the corresponding integer.
+    */
+    public func toInt () -> Int? {
+        return String(self).toInt()
+    }
+    
+}
